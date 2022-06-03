ab76ed9b9d3bfb0ec67aed812da2a448c10ffd10
Lee jh <asewe77@naver.com>
2022-06-03 14:29:06 +0900

    distance 수정

--- app/src/main/java/study/jyavanna/login_test2/Distance.java
+++ app/src/main/java/study/jyavanna/login_test2/Distance.java
@@ -6,49 +6,58 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class Distance extends AppCompatActivity {
     TextView textview1, textview2, textview3;
 
 
-    Button btnBD1;
-    Button btnBD2;
-    Button btnBD3;
-    Button btnBD4;
-    Button btnBD5;
-    Button btnBD6;
-    Button btnBD7;
-    Button btnBD8;
-    Button btnBD9;
-    Button btnBD10;
-    Button btnBD11;
-    Button btnBD12;
-    Button btnBD13;
+    ImageButton btnBD1;
+    ImageButton btnBD2;
+    ImageButton btnBD3;
+    ImageButton btnBD4;
+    ImageButton btnBD5;
+    ImageButton btnBD6;
+    ImageButton btnBD7;
+    ImageButton btnBD8;
+    ImageButton btnBD9;
+    ImageButton btnBD10;
+    ImageButton btnBD11;
+    ImageButton btnBD12;
+    ImageButton btnBD13;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_distance);
 
-        btnBD1 = (Button) findViewById(R.id.btnBD1);
-        btnBD2 = (Button) findViewById(R.id.btnBD2);
-        btnBD3 = (Button) findViewById(R.id.btnBD3);
-        btnBD4 = (Button) findViewById(R.id.btnBD4);
-        btnBD5 = (Button) findViewById(R.id.btnBD5);
-        btnBD6 = (Button) findViewById(R.id.btnBD6);
-        btnBD7 = (Button) findViewById(R.id.btnBD7);
-        btnBD8 = (Button) findViewById(R.id.btnBD8);
-        btnBD9 = (Button) findViewById(R.id.btnBD9);
-        btnBD10 = (Button) findViewById(R.id.btnBD10);
-        btnBD11 = (Button) findViewById(R.id.btnBD11);
-        btnBD12 = (Button) findViewById(R.id.btnBD12);
-        btnBD13 = (Button) findViewById(R.id.btnBD13);
+        btnBD1 = (ImageButton) findViewById(R.id.btnBD1);
+        btnBD2 = (ImageButton) findViewById(R.id.btnBD2);
+        btnBD3 = (ImageButton) findViewById(R.id.btnBD3);
+        btnBD4 = (ImageButton) findViewById(R.id.btnBD4);
+        btnBD5 = (ImageButton) findViewById(R.id.btnBD5);
+        btnBD6 = (ImageButton) findViewById(R.id.btnBD6);
+        btnBD7 = (ImageButton) findViewById(R.id.btnBD7);
+        btnBD8 = (ImageButton) findViewById(R.id.btnBD8);
+        btnBD9 = (ImageButton) findViewById(R.id.btnBD9);
+        btnBD10 = (ImageButton) findViewById(R.id.btnBD10);
+        btnBD11 = (ImageButton) findViewById(R.id.btnBD11);
+        btnBD12 = (ImageButton) findViewById(R.id.btnBD12);
+        btnBD13 = (ImageButton) findViewById(R.id.btnBD13);
 
         btnBD1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i1 = new Intent(Distance.this, WebBD1.class);
+                startActivity(i1);
+            }
+        });
+
+        btnBD2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i1 = new Intent(getApplicationContext(), WebBD2.class);
                 startActivity(i1);
             }
         });
--- app/src/main/java/study/jyavanna/login_test2/WebBD1.java
+++ app/src/main/java/study/jyavanna/login_test2/WebBD1.java
@@ -1,10 +1,10 @@
 package study.jyavanna.login_test2;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 public class WebBD1 extends AppCompatActivity {
 
@@ -13,10 +13,10 @@ public class WebBD1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_webbd1);
 
-        WebView webbd1 = (WebView) findViewById(R.id.webbd1);
-        webbd1.loadUrl("file:///android_assets/bd1.html");
+        WebView webView2 = (WebView) findViewById(R.id.webBdView1);
+        webView2.loadUrl("file:///android_asset/bd1.html");
 
-        WebSettings webSettings = webbd1.getSettings();
+        WebSettings webSettings = webView2.getSettings();
         webSettings.setJavaScriptEnabled(true);
     }
 }
